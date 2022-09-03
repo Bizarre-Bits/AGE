23d095be98c6d4cb5727ca0647decea7a18c84b2
nightingazer <nightingazer@outlook.com>
2022-09-03 20:16:32 +0200

    add abstract layer class and layer stack

---
+++ AGE/src/Age/Layer.cpp
@@ -0,0 +1,10 @@
+//
+// Created by alex on 29.08.22.
+//
+
+#include "Layer.h"
+
+namespace AGE {
+	Layer::Layer(const age_string_t &debugName) : debugName_(debugName) {}
+	Layer::~Layer() {}
+} // AGE
