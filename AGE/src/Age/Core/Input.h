f131c97d9b6b36cfcd191321bededb26166dce18
nightingazer <nightingazer@outlook.com>
2022-09-06 13:30:05 +0200

    add abstract input polling and OpenGL implementation

---
+++ AGE/src/Age/Core/Input.h
@@ -0,0 +1,33 @@
+//
+// Created by alex on 06.09.22.
+//
+
+#ifndef AGE_INPUT_H
+#define AGE_INPUT_H
+
+#include "Age/Core/Core.h"
+#include "Age/Core/KeyCodes.h"
+#include "Age/Core/MouseCodes.h"
+
+namespace AGE {
+  class DLL_PUBLIC Input {
+  public:
+    inline static bool IsKeyPressed(KeyCode keycode) { return s_Instance->IsKeyPressedImpl(keycode); }
+    inline static bool IsMousePressed(MouseCode mousecode) { return s_Instance->IsMousePressedImpl(mousecode); }
+    inline static std::pair<float, float> MousePos() { return s_Instance->MousePosImpl(); }
+    inline static float MouseX() { return s_Instance->MouseYImpl(); }
+    inline static float MouseY() { return s_Instance->MouseXImpl(); }
+
+  protected:
+    virtual bool IsKeyPressedImpl(KeyCode keycode) = 0;
+    virtual bool IsMousePressedImpl(MouseCode mousecode) = 0;
+    virtual std::pair<float, float> MousePosImpl() = 0;
+    virtual float MouseXImpl() = 0;
+    virtual float MouseYImpl() = 0;
+
+  private:
+    static Input* s_Instance;
+  };
+}// namespace AGE
+
+#endif//AGE_INPUT_H
---
+++ AGE/src/Platform/OpenGL/OpenGLInput.cpp
@@ -0,0 +1,45 @@
+//
+// Created by alex on 06.09.22.
+//
+
+#include "agepch.h"
+
+#include "Platform/OpenGL/PlatformGL.h"
+
+#include "Age/Core/Application.h"
+#include "Platform/OpenGL/OpenGLInput.h"
+
+
+namespace AGE {
+  Input* Input::s_Instance = new OpenGLInput;
+
+  bool OpenGLInput::IsKeyPressedImpl(KeyCode keycode) {
+    auto window = (GLFWwindow*)Application::Instance()->Window()->NativeWindow();
+
+    auto state = glfwGetKey(window, keycode);
+    return state == GLFW_PRESS || state == GLFW_REPEAT;
+  }
+
+  bool OpenGLInput::IsMousePressedImpl(MouseCode mousecode) {
+    auto window = (GLFWwindow*)Application::Instance()->Window()->NativeWindow();
+    int state = glfwGetMouseButton(window, mousecode);
+    return state == GLFW_PRESS;
+  }
+
+  std::pair<float, float> OpenGLInput::MousePosImpl() {
+    auto window = (GLFWwindow*)Application::Instance()->Window()->NativeWindow();
+    double xpos, ypos;
+    glfwGetCursorPos(window, &xpos, &ypos);
+    return {(float)xpos, (float)ypos};
+  }
+
+  float OpenGLInput::MouseXImpl() {
+    auto[x,y] = MousePosImpl();
+    return x;
+  }
+
+  float OpenGLInput::MouseYImpl() {
+    auto[x, y] = MousePosImpl();
+    return y;
+  }
+}// namespace AGE
